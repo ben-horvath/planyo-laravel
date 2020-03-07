16055ca12d3c36c6045b3bc743345ab6790e1eac
Ben <dev.ben.h@gmail.com>
2020-03-07 15:48:55 +0100

    Setup testing

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace benhorvath\PlanyoLaravel\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleFeatureTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
